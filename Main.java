633f7d178c0871e82484a677413d4760b8fa4659
andrebatistajunior <andrebatistajr030@gmail.com>
2026-08-28 14:34:23 -0300

    Implementa cadastro de múltiplos produtos

--- Main.java
+++ Main.java
@@ -1,31 +1,60 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
 
 
     public static void main(String[] args){
-        Produto produto1 = new Produto();
 
-        System.out.println("Informe o nome do produto: ");
+
+        int i = 0;
+
+        
+        ArrayList<Produto> produtos = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
 
+        
+        while(i < 2){
+
+        Produto produto = new Produto();
+
+        System.out.print("Informe o nome do produto: ");
+
         String nome = scanner.nextLine();
 
-        produto1.nome = nome;
+        produto.nome = nome;
 
-        System.out.println("Informe a quantidade do produto: ");
+
+        System.out.print("Informe a quantidade do produto disponível: ");
 
         int quantidade = scanner.nextInt();
 
-        produto1.quantidade = quantidade;
+        produto.quantidade = quantidade;
 
-        System.out.println(produto1.nome);
-        System.out.println(produto1.quantidade);
+        System.out.print("Informe o preco do produto: ");
 
-        
+        double preco = scanner.nextDouble();
+
+        scanner.nextLine();
+
+        produto.preco = preco;
+
+        produtos.add(produto);
+
+        i++;
+
+        }
+
+        for( i = 0 ; i < produtos.size(); i++){
+            System.out.println("Produto " + (i + 1));
+            System.out.println(produtos.get(i).nome);
+            System.out.println(produtos.get(i).quantidade);
+            System.out.println(produtos.get(i).preco);
+        }
 
        
 
+    
 
     }
     
