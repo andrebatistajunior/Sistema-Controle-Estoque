c03f24c16f6d2dd9ef64976ad7fca2f101cf0ed2
andrebatistajunior <andrebatistajr030@gmail.com>
2026-09-01 16:03:47 -0300

    Add product service class

--- Main.java
+++ Main.java
@@ -9,71 +9,12 @@ public class Main {
         int i = 0;
         
         
-        
-        ArrayList<Produto> produtos = new ArrayList<>();
+        ArrayList<ProdutoService> produtos = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
 
         int escolha;
 
-        do{
-
-
-        System.out.println("Escolha uma opção: 1- CADASTRAR PRODUTO  2- SAIR");
-        
-        escolha = scanner.nextInt();
-
-        scanner.nextLine();
-
-        switch(escolha){
-
-        case 1: 
-
-        Produto produto = new Produto();
-
-        System.out.print("Informe o nome do produto: ");
-
-        String nome = scanner.nextLine();
-
-        produto.nome = nome;
-
-        System.out.print("Informe a quantidade do produto disponível: ");
-
-        int quantidade = scanner.nextInt();
-    
-        produto.quantidade = quantidade;
-
-        if(produto.quantidade <= 20){
-            produto.estoque_baixo = true;
-
-        }if(produto.quantidade > 20){
-            produto.estoque_baixo = false;
-
-        }
-
-
-        System.out.print("Informe o preco do produto: ");
-
-        double preco = scanner.nextDouble();
-
-        scanner.nextLine();
-
-        produto.preco = preco;
-
-        produtos.add(produto);
-
-        break;
-
-        case 2:
-
-        escolha = 2;
-
-        break;
-
-
-    }
-
-        } while(escolha != 2);
         
     
         for( i = 0 ; i < produtos.size(); i++){
--- Produto.java
+++ Produto.java
@@ -1,7 +1,78 @@
-public class Produto {
+public class ProdutoService {
     String nome;
     int quantidade;
     double preco;
     boolean estoque_baixo;
 
 }
+
+public static void main(String[] args){
+    cadastrarProduto();
+
+}
+
+public static void cadastrarProduto{
+
+            do{
+
+
+        System.out.println("Escolha uma opção: 1- CADASTRAR PRODUTO  2- SAIR");
+        
+        escolha = scanner.nextInt();
+
+        scanner.nextLine();
+
+        switch(escolha){
+
+        case 1: 
+
+        ProdutoService produto = new ProdutoService();
+
+        System.out.print("Informe o nome do produto: ");
+
+        String nome = scanner.nextLine();
+
+        produto.nome = nome;
+
+        System.out.print("Informe a quantidade do produto disponível: ");
+
+        int quantidade = scanner.nextInt();
+    
+        produto.quantidade = quantidade;
+
+        if(produto.quantidade <= 20){
+            produto.estoque_baixo = true;
+
+        }if(produto.quantidade > 20){
+            produto.estoque_baixo = false;
+
+        }
+
+
+        System.out.print("Informe o preco do produto: ");
+
+        double preco = scanner.nextDouble();
+
+        scanner.nextLine();
+
+        produto.preco = preco;
+
+        produtos.add(produto);
+
+        break;
+
+        case 2:
+
+        escolha = 2;
+
+        break;
+
+
+    }
+
+        } while(escolha != 2);
+
+
+}
+
+
